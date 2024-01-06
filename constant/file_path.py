56799b0fc6ff34f070478c3f4f771e26b26d148a
ZhangBoyuan <byuan98@outlook.com>
2024-01-07 01:37:26 +0800

    feat: create constant

---
+++ constant/file_path.py
@@ -0,0 +1,8 @@
+# -*- codeing = utf-8 -*-
+# 文件路经常量
+# @Software: PyCharm
+# @File file_path.py
+# @Author : ZhangBoyuan
+# @Time : 2024/1/6 下午11:24
+
+db_url = 'sqlite:///database.db'
---
+++ constant/url_path.py
@@ -0,0 +1,8 @@
+# -*- codeing = utf-8 -*-
+# url地址路径常量
+# @Software: PyCharm
+# @File url_path.py
+# @Author : ZhangBoyuan
+# @Time : 2024/1/6 下午11:30
+
+get_url_list = "https://www.gov.cn/fuwu/zt/yqfwzq/qwcx.htm"
